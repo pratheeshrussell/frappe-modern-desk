09aaaab240b5046d5d7a0766d5ea9fff36417fb4
pratheeshrussell <pratheeshrussell@gmail.com>
2024-06-19 14:23:40 +0530

    workflow customizations: add doctype

---
+++ modern_desk/modern_desk/doctype/team_lunch/team_lunch.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Pratheesh Russell and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Team Lunch", {
+// 	refresh(frm) {
+
+// 	},
+// });
